ae8d502719012c86204ff59eb6cf89c44f8a1b8b
neelpatel024 <64968311+neelpatel024@users.noreply.github.com>
2020-10-02 03:21:05 -0700

    Create chat_screen.dart

---
+++ lib/pages/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../models/chat_model.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  ChatScreenState createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: (context, i) => new Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+              backgroundImage: new NetworkImage(
+                  "https://m.economictimes.com/thumb/msid-72269366,width-1200,height-900,resizemode-4,imgsize-331683/rohit-sharma-hits-the-ball-out-of-park-with-22-brands.jpg"),
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  dummyData[i].name,
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                ),
+                new Text(
+                  dummyData[i].time,
+                  style: new TextStyle(color: Colors.grey, fontSize: 14.0),
+                ),
+              ],
+            ),
+            subtitle: new Container(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: new Text(
+                dummyData[i].message,
+                style: new TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
